353a301dc932d54edb025243fd4190e07e4cc2d3
nmabenaca <n.maben@ardencahillacademy.com>
2023-01-09 19:25:48 +0000

    purple background

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -35,7 +35,7 @@ var background = function(window) {
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
             // width,      height,       color
-            var backgroundFill = draw.rect(canvasWidth, groundY, 'black');
+            var backgroundFill = draw.rect(canvasWidth, groundY, 'purple');
             backgroundFill.x = 0;
             backgroundFill.y = 0;
             background.addChild(backgroundFill);
