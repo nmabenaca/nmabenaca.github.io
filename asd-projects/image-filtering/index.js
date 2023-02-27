067d2a6fd6eff5bbaab1588b0c14d0528181af01
nmabenaca <n.maben@ardencahillacademy.com>
2023-02-27 20:21:32 +0000

    save image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -5,7 +5,6 @@ $(document).ready(function () {
   $("#apply").on("click", applyAndRender);
   $("#reset").on("click", resetAndRender);
 });
-
 /////////////////////////////////////////////////////////
 //////// event handler functions are below here /////////
 /////////////////////////////////////////////////////////
@@ -20,8 +19,10 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
+  applyFilter(reddify);
+  applyFilterNoBackground(decreaseBlue);
+  applyFilterNoBackground(increaseGreenByBlue);
 
-  
 
   // do not change the below line of code
   render($("#display"), image);
@@ -32,18 +33,62 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
-
+function applyFilter(filterFunction) {
+  for (var i = 0; i < image.length; i++) {
+    for (var j = 0; j < image[i].length; j++) {
+      var rgbString = image[i][j];
+      var rgbNumbers = rgbStringToArray(rgbString);
+      filterFunction(rgbNumbers);
+      rgbString = rgbArrayToString(rgbNumbers);
+      image[i][j] = rgbString;
+    }
+  }
+}
 
 // TODO 7: Create the applyFilterNoBackground function
 
+function applyFilterNoBackground(filterFunction) {
+  var backgroundColor = image[0][0];
+  for (var i = 0; i < image.length; i++) {
+    for (var j = 0; j < image[i].length; j++) {
+      
+      var currentColor = image[i][j];
+      
+      if (currentColor === backgroundColor){
+      }
+      else{
+        
+        var rgbNumbers = rgbStringToArray(currentColor);
+        filterFunction(rgbNumbers);
+        currentColor = rgbArrayToString(rgbNumbers);
+        image[i][j] = currentColor;        
+      }
+    }
+  }
+}
 
 // TODO 5: Create the keepInBounds function
+function keepInBounds(colorLimit) {
+  var result1 = Math.max(colorLimit, 0);
+  var result2 = Math.min(result1, 255)
+  return result2;
+
+}
+// console.log(keepInBounds(-30)); // should print 0
+// console.log(keepInBounds(300)); // should print 255
+// console.log(keepInBounds(127)); // should print 127
 
 
 // TODO 3: Create reddify function
-
+function reddify(rgbNumbers) {
+  rgbNumbers[RED] = 200;
+}
 
 // TODO 6: Create more filter functions
-
-
+function decreaseBlue(rgbNumbers) {
+  rgbNumbers[BLUE] = keepInBounds(rgbNumbers[BLUE] - 50)
+}
+function increaseGreenByBlue(rgbNumbers) {
+  rgbNumbers[GREEN] = keepInBounds(rgbNumbers[BLUE] + rgbNumbers[GREEN]);
+}
 // CHALLENGE code goes below here
